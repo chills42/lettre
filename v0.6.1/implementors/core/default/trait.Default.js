640162fa1f9d63e76e811937a7feb6c30e7da0a8
Alexis Mousset <contact@amousset.me>
2016-10-19 21:29:33 +0000

    Rebuild pages at 53f9bada4c9c50f4b3bd2f0fe5c917ce1de2b6ac

---
+++ v0.6.1/implementors/core/default/trait.Default.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["lettre"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/default/trait.Default.html' title='core::default::Default'>Default</a> for <a class='struct' href='lettre/transport/smtp/response/struct.ResponseParser.html' title='lettre::transport::smtp::response::ResponseParser'>ResponseParser</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/default/trait.Default.html' title='core::default::Default'>Default</a> for <a class='struct' href='lettre/email/struct.SimpleEmail.html' title='lettre::email::SimpleEmail'>SimpleEmail</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ v0.6.1/implementors/core/hash/trait.Hash.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["lettre"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/hash/trait.Hash.html' title='core::hash::Hash'>Hash</a> for <a class='enum' href='lettre/transport/smtp/extension/enum.Extension.html' title='lettre::transport::smtp::extension::Extension'>Extension</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/hash/trait.Hash.html' title='core::hash::Hash'>Hash</a> for <a class='enum' href='lettre/transport/smtp/authentication/enum.Mechanism.html' title='lettre::transport::smtp::authentication::Mechanism'>Mechanism</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
