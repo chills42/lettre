546e289bd4f2d7c71c86f4f93776d34a4f409449
Alexis Mousset <contact@amousset.me>
2016-10-23 19:19:07 +0000

    Rebuild pages at f8f024ae7c5b71d41310b59f637ced6309f99376

--- master/lettre/transport/sendmail/error/sidebar-items.js
+++ master/lettre/transport/sendmail/error/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"enum":[["Error","An enum of all error kinds."]],"type":[["SendmailResult","SendMail result type"]]});
+initSidebarItems({"enum":[["Error","An enum of all error kinds."]],"type":[["SendmailResult","sendmail result type"]]});
